5d1d3ae01d35f98c5c94480c90990b38de6623d6
WhoSayin52 <180821950+WhoSayin52@users.noreply.github.com>
2026-02-18 12:40:46 +0100

    successfully rendered the model correctly. Still need to implement clipping and buffer bound checking as to avoid drawing non-visible triangles and touching out of bound triangles

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -4,17 +4,18 @@
 
 #include <cstdlib>
 
+// internal consts
+static constexpr Vector3 COLOR_WHITE = { 1.0f, 1.0f, 1.0f };
+
 // internal structs
-struct Basis {
-	Vector3 x;
-	Vector3 y;
-	Vector3 z;
-};
 struct Camera {
 	Vector3 position;
-	Basis basis;
-	f32 fov; // field of view;
+	Vector3 right;
+	Vector3 up;
+	Vector3 forward;
+	f32 fov; // field of view in radians;
 	f32 near; // distance between camera and viewport
+	f32 aspect_ratio;
 };
 
 struct Entity {
@@ -23,23 +24,26 @@ struct Entity {
 };
 
 // static global vars
-static Camera camera = {
+static Camera global_camera = {
 	.position = Vector3{0, 0, 0},
-	.basis = Basis{
-		.x = Vector3{1, 0, 0},
-		.y = Vector3{0, 1, 0},
-		.z = Vector3{0, 0, 1}
-	},
-	.fov = 60.0f, // degrees
-	.near = 1.0f // 1 for simplicity 
+	.right = Vector3{1, 0, 0},
+	.up = Vector3{0, 1, 0},
+	.forward = Vector3{0, 0, 1},
+
+	.fov = 60.0f * Math::PI32 / 180.0f, // radians
+	.near = 1.0f, // 1 for simplicity 
+	.aspect_ratio = 16.0f / 9.0f
 };
 static Entity global_diablo_entity = {
-	.position = Vector3{0, 0, 1000},
+	.position = Vector3{0, 0, 3.5f},
 	.model = Model{}
 };
 
 // internal functions
-static void to_camera_space(Camera* camera, Vector3* vertices, i64 vertices_count);
+static void draw_entity(Memory::Arena* arena, Canvas* canvas, Camera* camera, Entity* entity);
+static void draw_triangles(Canvas* canvas, Camera* camera, Entity* entity, Vector3* vba);
+static Vector3 project_to_viewport(Camera* camera, Vector3 vertex);
+static void local_to_camera_space(Camera* camera, Entity* entity, Vector3* vba);
 static void draw_line(Canvas* canvas, Vector2i p0, Vector2i p1, Vector3 color);
 static void set_pixel(Canvas* canvas, i32 x, i32 y, Vector3 color);
 
@@ -54,20 +58,76 @@ void init_renderer(Renderer_Memory* memory, wchar* path_to_assets) {
 }
 
 void render(Memory::Arena* arena, Canvas* canvas) {
+	draw_entity(arena, canvas, &global_camera, &global_diablo_entity);
+}
+
+static void draw_entity(Memory::Arena* arena, Canvas* canvas, Camera* camera, Entity* entity) {
 
 	Memory::Arena_Snapshot arena_snapshot = Memory::arena_create_snapshot(arena);
 
-	i64 vertices_count = global_diablo_entity.model.vertices_count;
-	Vector3* vertices = (Vector3*)Memory::arena_push(arena, vertices_count * sizeof(Vector3), alignof(Vector3));
+	i64 diablo_vba_count = global_diablo_entity.model.vertices_count;
+	Vector3* diablo_vba = (Vector3*)Memory::arena_push(arena, diablo_vba_count * sizeof(Vector3), alignof(Vector3));
 
-
+	local_to_camera_space(camera, &global_diablo_entity, diablo_vba);
+	draw_triangles(canvas, camera, entity, diablo_vba);
 
 	Memory::arena_restore(arena_snapshot);
 }
 
-static void to_camera_space(Camera* camera, Vector3* vertices, i64 vertices_count, Entity* entity){
-	
-	//Vector3* vertex = entity
+static void draw_triangles(Canvas* canvas, Camera* camera, Entity* entity, Vector3* vba) {
+	Vector2 viewport;
+	viewport.w = (2 * (tanf(camera->fov / 2.0f) * camera->near));
+	viewport.h = viewport.w / camera->aspect_ratio;
+
+	Vector2 canvas_viewport_ratio;
+	canvas_viewport_ratio.x = canvas->w / viewport.w;
+	canvas_viewport_ratio.y = canvas->h / viewport.h;
+
+	Vector3 v1, v2, v3;
+	Vector2i p1, p2, p3;
+	Face* faces = entity->model.faces;
+	i64 faces_count = entity->model.faces_count;
+	for (i64 i = 0; i < faces_count; ++i) {
+		v1 = project_to_viewport(camera, vba[faces[i].v_indices[0]]);
+		v2 = project_to_viewport(camera, vba[faces[i].v_indices[1]]);
+		v3 = project_to_viewport(camera, vba[faces[i].v_indices[2]]);
+
+		p1.x = (i32)roundf(v1.x * canvas_viewport_ratio.x);
+		p1.y = (i32)roundf(v1.y * canvas_viewport_ratio.y);
+
+		p2.x = (i32)roundf(v2.x * canvas_viewport_ratio.x);
+		p2.y = (i32)roundf(v2.y * canvas_viewport_ratio.y);
+
+		p3.x = (i32)roundf(v3.x * canvas_viewport_ratio.x);
+		p3.y = (i32)roundf(v3.y * canvas_viewport_ratio.y);
+
+		draw_line(canvas, p1, p2, COLOR_WHITE);
+		draw_line(canvas, p1, p3, COLOR_WHITE);
+		draw_line(canvas, p2, p3, COLOR_WHITE);
+	}
+}
+
+static Vector3 project_to_viewport(Camera* camera, Vector3 vertex) {
+	Vector3 vn = Math::normalize(vertex);
+
+	f32 t = camera->near / vn.z;
+	Vector3 result;
+	result.x = vn.x * t;
+	result.y = vn.y * t;
+	result.z = vertex.z - camera->near;
+
+	return result;
+}
+
+static void local_to_camera_space(Camera* camera, Entity* entity, Vector3* vba) {
+
+	i64 count = entity->model.vertices_count;
+	Vector3* vertices = entity->model.vertices;
+
+	Vector3 camera_relative_position = entity->position - camera->position;
+	for (i64 i = 0; i < count; ++i) {
+		vba[i] = vertices[i] + camera_relative_position;
+	}
 }
 
 static void draw_line(Canvas* canvas, Vector2i p0, Vector2i p1, Vector3 color) {
